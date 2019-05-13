33751e2771a1bb2f6ce24b221d4060e5100a73e5
vitorquintino <vitor_quintino@usp.br>
2019-05-13 20:27:47 -0300

    Criando umas pthreads

--- main.c
+++ main.c
@@ -1,26 +1,45 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <string.h>
 
-void* threadFunction(void* args);
+const char* requests[5];
+int i = 0; 
+
+void* threadFunction(const char* requestString);
 
 int main(){
-	printf("Hello, World!\n");
-	pthread_t id;
-	int ret;
+	requests[0] = "Requisicoes";
+	requests[1] = "Que";
+	requests[2] = "Serao";
+	requests[3] = "Invertidas";
+	requests[4] = "No pthread";
 
-	ret = pthread_create(&id, NULL, &threadFunction, NULL);
-	if(ret==0){
-        printf("Thread created successfully.\n");
-    }
-    else{
-        printf("Thread not created.\n");
-        return 0;
-    }
+	for(i = 0; i < 5; i++){
+		pthread_t thread;
+		int threadCreated;
+
+		threadCreated = pthread_create(&thread, NULL, threadFunction(requests[i]), NULL);
+		/*if(threadCreated == 0){
+	        printf("PThread %d created successfully.\n", i);
+	    }
+	    else{
+	        printf("PThread %d not created.\n", i);
+	    }*/
+	}
 
 	return 0;
 }
 
 
-void* threadFunction(void* args){
-    printf("I am threadFunction.\n");
+void* threadFunction(const char* requestString){
+	printf("PThread %d created successfully.\n", i);
+    
+    printf("Requisicao antes de inverter: %s\n", requestString);
+	int tamanhoString = strlen(requestString), iterador;
+
+	for(iterador = tamanhoString - 1; iterador >= 0; iterador--){
+		printf("%c", requestString[iterador]);
+	}
+
+    printf("\nPThread %d ended successfully.\n", i);
 }
